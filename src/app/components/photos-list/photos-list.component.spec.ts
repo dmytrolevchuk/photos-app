62093b08c186baaa2c360b582b1bf27271bf1166
Dmytro Levchuk <Dmytro_Levchuk@epam.com>
2022-05-05 18:41:25 +0300

    add infinite scroll and test cases

--- src/app/components/photos-list/photos-list.component.spec.ts
+++ src/app/components/photos-list/photos-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from "@angular/core";
 
@@ -7,7 +7,8 @@ import { PhotosListComponent } from './photos-list.component';
 describe('PhotosListComponent', () => {
   let component: PhotosListComponent;
   let fixture: ComponentFixture<PhotosListComponent>;
-  let de: DebugElement[];
+  let dePhotos: DebugElement[];
+  let deIcon: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,8 +28,42 @@ describe('PhotosListComponent', () => {
   });
 
   it('should be 10 images at the starting point', () => {
-    fixture.detectChanges();
-    de = fixture.debugElement.queryAll(By.css('img'));
-    expect(de.length).toBe(10);
+    dePhotos = fixture.debugElement.queryAll(By.css('mat-grid-list img'));
+    expect(dePhotos.length).toBe(10);
   })
+
+  it('should not be loading icon at the start', () => {
+    // const container = fixture.debugElement.query(By.css('mat-grid-list'));
+    //
+    // container.nativeElement.scrollLeft = 50;
+    // expect(container.nativeElement.scrollLeft).toEqual(50);
+    dePhotos = fixture.debugElement.queryAll(By.css('img[src="/assets/images/loading.png"]'));
+    expect(dePhotos.length).toBe(0);
+    // window.dispatchEvent(new Event("scroll"));
+    // window.scrollTo(0 , 50);
+  })
+
+  it('should be loading icon after scrolling', () => {
+    // const container = fixture.debugElement.query(By.css('mat-grid-list'));
+    //
+    // container.nativeElement.scrollLeft = 50;
+    // expect(container.nativeElement.scrollLeft).toEqual(50);
+    component.loadPhotos();
+    fixture.detectChanges();
+    deIcon = fixture.debugElement.query(By.css('img[src="/assets/images/loading.png"]'));
+    expect(deIcon).toBeDefined();
+
+  })
+
+  it('should not be loading icon after timeout and be 20 photos', fakeAsync(() => {
+    component.loadPhotos();
+    fixture.detectChanges();
+    tick(4000);
+    fixture.detectChanges();
+    deIcon = fixture.debugElement.query(By.css('img[src="/assets/images/loading.png"]'));
+    expect(deIcon).toBeNull();
+    dePhotos = fixture.debugElement.queryAll(By.css('mat-grid-list img'));
+    expect(dePhotos.length).toBe(20);
+
+  }))
 });
--- src/app/components/photos-list/photos-list.component.ts
+++ src/app/components/photos-list/photos-list.component.ts
@@ -1,19 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, OnDestroy} from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { tap, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photos-list',
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.scss']
 })
-export class PhotosListComponent {
+export class PhotosListComponent implements OnInit, OnDestroy {
   mockedList: number[];
   amount: number = 10;
+  showLoading: boolean = false;
+  scrollSub!: Subscription;
 
   constructor() {
     this.mockedList = Array(this.amount).fill(0).map((x,i)=>i);
   }
 
+  ngOnInit() {
+    this.scrollSub = fromEvent(window, 'scroll').pipe(
+      debounceTime(1000),
+      tap(() => this.checkScrolling())
+    ).subscribe();
+  }
+
+  checkScrolling(): void {
+    const fullHeight = (this.amount/2) * 300 + 50;
+    if ( window.scrollY + window.outerHeight > fullHeight) {
+      this.loadPhotos();
+    }
+  }
+
   savePhoto(index: number) {
     console.log(index);
+  }
+
+  loadPhotos(): void {
+    this.showLoading = true;
+    let timer = 3000;
+    setTimeout(() => {
+      for (let i = this.amount; i < this.amount + 10; i++) {
+        this.mockedList.push(i);
+      }
+      this.amount = this.mockedList.length;
+      this.showLoading = false;
+    }, timer);
+  }
+
+  ngOnDestroy() {
+    this.scrollSub.unsubscribe();
   }
 }
